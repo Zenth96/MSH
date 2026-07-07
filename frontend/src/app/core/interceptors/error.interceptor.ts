c2962a51f77c59d90d2fca3cc55fb79421ee972d
Miskolczi Levente <mlevente05@gmail>
2026-07-07 19:25:45 +0200

    feat(frontend-core): add auth service, API service, interceptors

---
+++ frontend/src/app/core/interceptors/error.interceptor.ts
@@ -0,0 +1,18 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { catchError, throwError } from 'rxjs';
+
+export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+  const auth = inject(AuthService);
+
+  return next(req).pipe(
+    catchError(err => {
+      if (err.status === 401) {
+        auth.logout();
+      }
+
+      return throwError(() => err);
+    }),
+  );
+};
